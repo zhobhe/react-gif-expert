a68969c879aec135938ea229f10417cb96cb1a19
zhobhe <zhobhe@gmail.com>
2023-02-07 15:27:35 +0100

    Update getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) =>{
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=GJ43aGrNSKbkuu8ya9t3Z9xX7inf8sRP&q=${category}&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=GJ43aGrNSKbkuu8ya9t3Z9xX7inf8sRP&q=${category}&limit=30`
     const resp = await fetch (url);
     const {data}= await resp.json();
     
